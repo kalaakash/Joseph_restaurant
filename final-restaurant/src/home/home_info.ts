e265af986defd2a0b472ab90f1a0aeed3e2510e2
kalaakash <kalaaki@gmail.com>
2020-02-06 16:08:26 +0000

    Make changes according to new requirements from PT uncle

--- final-restaurant/src/home/home_info.ts
+++ final-restaurant/src/home/home_info.ts
@@ -1,8 +1,9 @@
 export const HomeInfo: String[]
     = [
-        "EXPERIENCE THE MAGIC OF TASTE",
-        "You love pizza? We do too !!",
-        "We've got your faviorite softy ice-cream",
-        "Come down for a delicious meal with BURGERS & PASTA."
+        "Pizza",
+        "Burger",
+        "Falooda",
+        "Waffles",
+        "French Fries"
     ];
 
