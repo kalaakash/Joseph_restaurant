9efd12c5cb62bf76eed670d3f85514a57b78c196
kalaakash <kalaaki@gmail.com>
2020-02-06 19:03:23 +0000

    Adjust the home and menu as needed

--- final-restaurant/src/menu/Food_items.ts
+++ final-restaurant/src/menu/Food_items.ts
@@ -3,82 +3,453 @@ import { Food_item } from './Food_item';
 export const FOODITEMS: Food_item[] = [
     {
         _id: 1,
-        name: "Chicken Burger",
-        category: "Main Course"
+        name: "Vanilla : ₹30(Regular) , ₹50(Large)",
+        category: "Softy Cones"
     },
     {
         _id: 2,
-        name: "Vegetarian Burger",
-        category: "Main Course"
+        name: "Strawberry/Chocolate : ₹30(Regular) , ₹50(Large)",
+        category: "Softy Cones"
     },
     {
         _id: 3,
-        name: "Chicken Sandwich",
-        category: "Snacks"
+        name: "Vanilla + Chocolate/Strawberry : ₹30(Regular) , ₹50(Large)",
+        category: "Softy Cones"
     },
     {
         _id: 4,
-        name: "Vegetarian Sandwich",
-        category: "Snacks"
+        name: "Vanilla Choco Dip : ₹40(Regular) , ₹60(Large)",
+        category: "Softy Cones"
     },
     {
         _id: 5,
-        name: "Cheese Burger",
-        category: "Main Course"
+        name: "Ferrero Rocher : ₹80(Large)",
+        category: "Softy Cups"
     },
     {
         _id: 6,
-        name: "Chicken Pizza",
-        category: "Snacks"
+        name: "Chocolate Cookies : ₹40(Regular), ₹70(Large)",
+        category: "Softy Cups"
     },
     {
         _id: 7,
-        name: "Vegetarian Pizza",
-        category: "Snacks"
+        name: "Caramel Popcorn : ₹40(Regular), ₹70(Large)",
+        category: "Softy Cups"
     },
     {
         _id: 8,
-        name: "Cheese Pizza",
-        category: "Snacks"
+        name: "Sprinkles : ₹40(Regular), ₹70(Large)",
+        category: "Softy Cups"
     },
     {
         _id: 9,
-        name: "Chicken Pasta",
-        category: "Snacks"
+        name: "Chocolate Chips : ₹40(Regular), ₹70(Large)",
+        category: "Softy Cups"
     },
     {
         _id: 10,
-        name: "Vegetarian Pasta",
-        category: "Snacks"
+        name: "Vanilla : ₹30(Regular), ₹50(Double), ₹80(Triple)",
+        category: "Ice Cream Scoops"
     },
     {
         _id: 11,
-        name: "Pesto Pasta",
-        category: "Snacks"
+        name: "Strawberry : ₹40(Regular), ₹70(Double), ₹110(Triple)",
+        category: "Ice Cream Scoops"
     },
     {
         _id: 12,
-        name: "Chicken Wrap",
-        category: "Snacks"
+        name: "Chocolate : ₹40(Regular), ₹70(Double), ₹110(Triple)",
+        category: "Ice Cream Scoops"
     },
     {
         _id: 13,
-        name: "Vegetarian Wrap",
-        category: "Snacks"
+        name: "Tender Coconut : ₹70(Regular), ₹130(Double), ₹200(Triple)",
+        category: "Ice Cream Scoops"
     },
     {
         _id: 14,
-        name: "Chocolate Ice Cream",
-        category: "Softy & Ice-Creams"
+        name: "Mango : ₹50(Regular), ₹90(Double), ₹140(Triple)",
+        category: "Ice Cream Scoops"
     },
     {
         _id: 15,
-        name: "Vanilla Ice Cream",
-        category: "Softy & Ice-Creams"
+        name: "Pistachio : ₹50(Regular), ₹90(Double), ₹140(Triple)",
+        category: "Ice Cream Scoops"
     },
     {
         _id: 16,
-        name: "Pista Ice Cream",
-        category: "Softy & Ice-Creams"
-    }
+        name: "Chicoo : ₹70(Regular), ₹130(Double), ₹200(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 17,
+        name: "Coffee : ₹40(Regular), ₹70(Double), ₹110(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 18,
+        name: "Passionfruit : ₹70(Regular), ₹130(Double), ₹200(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 19,
+        name: "Butter scotch : ₹50(Regular), ₹90(Double), ₹140(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 20,
+        name: "Keser Badam Pista : ₹60(Regular), ₹110(Double), ₹170(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 21,
+        name: "Spanish Delight : ₹60(Regular), ₹110(Double), ₹170(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 22,
+        name: "Jackfruit : ₹70(Regular), ₹130(Double), ₹200(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 23,
+        name: "Fig & Honey : ₹70(Regular), ₹130(Double), ₹200(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 24,
+        name: "Blue Berry : ₹80(Regular), ₹150(Double), ₹230(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 25,
+        name: "Black Current : ₹60(Regular), ₹110(Double), ₹170(Triple)",
+        category: "Ice Cream Scoops"
+    },
+    {
+        _id: 26,
+        name: "Chocolate mint Fudge : ₹80",
+        category: "Sundaes"
+    },
+    {
+        _id: 27,
+        name: "Championship Chocolate : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 28,
+        name: "Chocolate cake : ₹80",
+        category: "Sundaes"
+    },
+    {
+        _id: 29,
+        name: "Banana Split : ₹120",
+        category: "Sundaes"
+    },
+    {
+        _id: 30,
+        name: "Banana Boat : ₹150",
+        category: "Sundaes"
+    },
+    {
+        _id: 31,
+        name: "Death by Chocolate : ₹150",
+        category: "Sundaes"
+    },
+    {
+        _id: 32,
+        name: "Royal Butter Scotch : ₹100",
+        category: "Sundaes"
+    },
+    {
+        _id: 33,
+        name: "Nutling Caramel : ₹80",
+        category: "Sundaes"
+    },
+    {
+        _id: 34,
+        name: "Nulty : ₹90",
+        category: "Sundaes"
+    },
+    {
+        _id: 35,
+        name: "Pink Drip (Strawberry) : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 36,
+        name: "Ocean Blue (Blueberry) : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 37,
+        name: "Cherry Chalice : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 38,
+        name: "Premier Pineapple : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 39,
+        name: "Vanilla Coffee : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 40,
+        name: "Vanilla Butterscotch : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 41,
+        name: "Vanilla Mint : ₹70",
+        category: "Sundaes"
+    },
+    {
+        _id: 42,
+        name: "Royal Falooda : ₹120",
+        category: "Falooda"
+    },
+    {
+        _id: 43,
+        name: "Taste MagiQ Falooda : ₹150",
+        category: "Falooda"
+    },
+    {
+        _id: 44,
+        name: "Fruit Salad : ₹80",
+        category: "Fruit Salad"
+    },
+    {
+        _id: 45,
+        name: "Fruit Salad with Ice Cream : ₹100",
+        category: "Fruit Salad"
+    },
+    {
+        _id: 46,
+        name: "Sharjah Shake : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 47,
+        name: "Vanilla : ₹80",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 48,
+        name: "Strawberry : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 49,
+        name: "Mango : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 50,
+        name: "Pistachio : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 51,
+        name: "Black Current : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 52,
+        name: "Chocolate : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 53,
+        name: "Coffee : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 54,
+        name: "Butter scotch : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 55,
+        name: "Keser Badam Pista : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 56,
+        name: "Spanish Delight : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 57,
+        name: "Jackfruit : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 58,
+        name: "Lychee : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 59,
+        name: "Blueberry : ₹120",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 60,
+        name: "Kitkat Milkshake : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 61,
+        name: "Hide & Seek Milkshake : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 62,
+        name: "Oreo Shake : ₹90",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 63,
+        name: "Kiwi : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 64,
+        name: "Kulfi : ₹100",
+        category: "Milk Shakes"
+    },
+    {
+        _id: 65,
+        name: "Yummy Mango : ₹120",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 66,
+        name: "Pink Guava : ₹120",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 67,
+        name: "Chickoo : ₹120",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 68,
+        name: "Strawberry : ₹120",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 69,
+        name: "Pomogranate : ₹120",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 70,
+        name: "Taste MagiQ Shake : ₹120",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 71,
+        name: "Taste MagiQ Double Shake : ₹130",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 72,
+        name: "Freak Shake : ₹150",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 73,
+        name: "Blue Berry : ₹200",
+        category: "Thick Shakes"
+    },
+    {
+        _id: 74,
+        name: "Alphonso Mango : ₹120",
+        category: "Smoothies"
+    },
+    {
+        _id: 75,
+        name: "Pink Guava : ₹120",
+        category: "Smoothies"
+    },
+    {
+        _id: 76,
+        name: "Strawberry : ₹120",
+        category: "Smoothies"
+    },
+    {
+        _id: 77,
+        name: "Sitafal (Custard Apple) : ₹120",
+        category: "Smoothies"
+    },
+    {
+        _id: 78,
+        name: "Chickoo : ₹120",
+        category: "Smoothies"
+    },
+    {
+        _id: 79,
+        name: "Taste MagiQ double : ₹120",
+        category: "Smoothies"
+    },
+    {
+        _id: 80,
+        name: "Alphonso Mango : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Pink Guava : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Pomogranate : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Orange : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Mosambi : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Lime Juice : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Lime Soda : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Black Coffee : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Cappucino : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 80,
+        name: "Mochacinno : ₹120",
+        category: "Fruit Juices"
+    },
+    {
+        _id: 90,
+        name: "H : ₹120",
+        category: "Fruit Juices"
+    },
+
 ];
--- final-restaurant/src/menu/menu.component.ts
+++ final-restaurant/src/menu/menu.component.ts
@@ -11,13 +11,34 @@ export class MenuComponent implements OnInit {
 
   FoodItems = FOODITEMS;
   categories:String[];
+  categories2: string[];
 
   constructor() { 
     this.categories = [
-      "Snacks",
-      "Main Course",
-      "Softy & Ice-Creams"
+      "Softy Cones",
+      "Softy Cups",
+      "Ice Cream Scoops",
+      "Sundaes",
+      "Falooda",
+      "Fruit Salad",
+      "Milk Shakes",
+      "Thick Shakes",
+      "Smoothies",
+      "Fruit Juices",
+      "Beverages"    
     ];
+    this.categories2 = [
+      "Softy & Ice-Creams",
+      "Waffles",
+      "Popcorn",
+      "Pizza Veg",
+      "Pizza Non-Veg",
+      "Calzone",
+      "Toppings",
+      "Bread",
+      "Burgers",
+      "Sides"
+    ]
   }
 
   ngOnInit() {
